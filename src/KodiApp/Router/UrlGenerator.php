02b3444266963b78b3de6de3f51a5fc610f294f0
nagyatka <nagyatka@gmail.com>
2016-12-21 14:24:26 +0100

    Urlgenerator bugfix

--- src/KodiApp/Router/UrlGenerator.php
+++ src/KodiApp/Router/UrlGenerator.php
@@ -9,6 +9,7 @@
 namespace KodiApp\Router;
 
 
+use Exception;
 use KodiApp\Application;
 use KodiApp\Translator\Translator;
 
@@ -42,7 +43,7 @@ class UrlGenerator
         $resultUrl = "";
         $index = 0;
         while(($firstPos = strpos($url,"{",$index)) != false) {
-            $lastPos = strpos($url,"}",$firstPos);
+            $lastPos = $this->getPositionOfPairCurlyBrackets($url,$firstPos);
             $name = explode(":",substr($url,$firstPos+1,($lastPos-$firstPos-1)))[0];
             $resultUrl .= substr($url,$index,$firstPos-$index).(isset($parameters[$name])?$parameters[$name]:"");
             $index = $lastPos+1;
@@ -56,6 +57,30 @@ class UrlGenerator
         }
         return $resultUrl;
 
+    }
+
+    private function getPositionOfPairCurlyBrackets($str,$start) {
+
+        $openingCount = 1;
+
+        $index = $start + 1;
+        while($openingCount != 0) {
+            // Megnézzük, hogy hol van a kövi záró
+            $nextIndex = strpos($str,"}",$index);
+            if($index == false) {
+                throw new Exception("Parse error in route (missing } ): ".$str);
+            } else {
+                // Egy nyitott zárójelhez biztosan találtunk párt.
+                $openingCount--;
+            }
+            // Megnézzük, hogy közben mennyi nyitó volt
+            $openingCount += substr_count(substr($str,$index,$nextIndex-$index),"{");
+
+            //Növeljük az indexet
+            $index = $nextIndex+1;
+
+        }
+        return $index-1;
     }
 }
 
